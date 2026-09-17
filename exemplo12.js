71cc9faa8613f12d9720b37db7a1ddaa028dc697
VICTOR STEPHAN LULU VALENTIM <victor_s_valentim@estudante.sesisenai.org.br>
2026-09-17 18:59:32 -0300

    exemplo 12 atualizado

--- exemplo12.js
+++ exemplo12.js
@@ -1,8 +1,17 @@
 const frutas = ['Maçã', 'Banana', 'Uva'];
+console.log('array original:', frutas);
+ 
 
+frutas.splice(2, 1);
+console.log('removeu 1 item:', frutas);
+ 
 
+frutas.splice(2, 0, 'Tomate');
+console.log('adicionou 1 item:', frutas);
+ 
 
-//for (const fruta of frutas) {
-//  console.log(fruta);
-//}
+frutas.splice(0, 1, 'Morango');
+console.log('substituiu 1 item:', frutas);
+ 
 
+console.log('Array final:', frutas);
